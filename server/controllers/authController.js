54f8852a28e67341636b99706f303826b28ca5f6
Shubham Jha <shubhamjha03563@gmail.com>
2021-09-21 00:19:31 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -34,7 +34,7 @@ exports.login = asyncHandler(async (req, res, next) => {
 
   // if user not found or password is wrong
   if (!user || !(await user.matchPassword(password))) {
-    res.status(401).json({
+    res.status(200).json({
       message: 'Wrong login credentials',
     });
     return next();
