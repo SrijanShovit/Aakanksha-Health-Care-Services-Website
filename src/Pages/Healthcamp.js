a787b059c10598e21d9bfa019576b746915a7f5e
Srijan Shovit <srijanshovit@gmail.com>
2021-09-24 10:58:08 +0530

    healthcamp old and new

--- src/Pages/Healthcamp.js
+++ src/Pages/Healthcamp.js
@@ -3,6 +3,7 @@ import {Button, Container,Card,Row,Col} from "react-bootstrap";
 import Header from '../Components/Navbar'
 import Footer from '../Components/Footer'
 import Popup from '../Components/popup'
+import doctor1 from '../Images/doctor1.jpg'
 import Popup1 from '../Components/popuplogin'
 import Axios from 'axios';
 const Healthcamp = () => {
@@ -42,6 +43,58 @@ const Healthcamp = () => {
                 ))}
            </Row>
         </Container>
+
+        <div className="container" align="center">
+                        <div className="col-lg-12 col-12 my-2 card">
+
+                            <div className="row mt-3">
+                               
+
+                                    {
+                                        data.map( item => 
+                                        (
+                                            <>
+                                            <div className="container"></div>
+                                            <div className="col-lg-4 col-12 doctordetail doctorphoto" style={{ padding: "5px" }} align="center">
+                                                
+                                            <img src={item.imageUrl} className="d-block mt-3 cardgroup" alt="medicine" />
+                                </div>
+                                <div className="col-lg-8 col-12" align="center">
+
+                                    <div className="card-body mt-3" align="left">
+                                        <h5 className="card-title">{item.name}</h5>
+                                        <h6>{item.city}</h6>
+
+                                        <p className="card-text description">
+                                            {item.description}
+
+                                        </p>
+                                        <p style={{ padding: "3px" }}>
+                                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio accusamus cum neque cumque veniam repellendus nihil corrupti, earum delectus laborum tempore perspiciatis autem exercitationem? Voluptas corrupti, ut laborum repudiandae nihil repellat quae inventore ad blanditiis delectus deleniti, provident vitae magnam voluptatum eius, dolor facilis tempora minus fuga. Quidem, facilis illum.
+                                        </p>
+                                    </div>
+
+                                    <div align="right">
+
+                                        <button type="button" className="btn btn-primary seemore1 mx-3 my-2">Go to Location</button>
+                                            
+                                    </div>
+                                    </div>
+                                    </>
+                                            )
+                                        )
+
+                                    }
+                                    
+
+
+                                
+                            </div>
+                        </div>
+
+                    </div>
+
+
         <Footer/>
     </div>
     );
