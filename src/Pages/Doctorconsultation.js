6e4acb5b93ba760c0e8354b0fed16dd020244cd6
Shweta Birthare <shweta9birthare@gmail.com>
2021-10-11 18:48:14 -0400

    make consulattion page dynamic and implement search functionality

--- src/Pages/Doctorconsultation.js
+++ src/Pages/Doctorconsultation.js
@@ -389,22 +389,51 @@
 // export default Doctorconsultation
 
 
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Header from '../Components/Navbar'
 import Footer from '../Components/Footer'
 import Popup from '../Components/popup'
 import Popup1 from '../Components/popuplogin'
-import doctor1 from '../Images/doctor1.jpg'
-import doctor2 from '../Images/doctor2.jpg'
-import doctor3 from '../Images/doctor3.jpg'
+//import doctor1 from '../Images/doctor1.jpg'
+//import doctor2 from '../Images/doctor2.jpg'
+//import doctor3 from '../Images/doctor3.jpg'
 import app from '../Images/appointment.png'
 import always from '../Images/24x7.jpg'
 import anytime from '../Images/anytime.png'
 import allcategory from '../Images/allcategory.jfif'
 import { FaSearch } from "react-icons/fa";
-import {Container} from "react-bootstrap";
+import {Container,Spinner} from "react-bootstrap";
 import InfoModal from "../Components/Bookapp/detailsForm"
+import Axios from 'axios';
 const Doctorconsultation = () => {
+    const [data, setData] = useState([]);
+    const [loading,setloading]=useState(false);
+    const [keyword,setKeyword]=useState("");
+    const style = { position: "fixed",  left: "50%" ,top:"50%"};
+    useEffect(() => {
+        function fetchData () {
+            Axios.post('http://localhost:5000/doctor/getDoctorDetail')
+            .then((response)=>{
+               
+                setData(response.data.doctors);
+                setloading(true);
+            })
+        };
+        fetchData();
+      },[setData]);
+    //function to search products
+    const searchProduct=()=>{
+       
+        Axios.post('http://localhost:5000/doctor/searchDoctors',
+        {
+          "searchQuery":keyword,
+            "page":1,
+            "pageLimit":5
+         }).then((response)=>
+         {
+           setData(response.data.results);
+        })
+     }
     return (
         <>
             <Popup />
@@ -412,9 +441,11 @@ const Doctorconsultation = () => {
             <Header />
             <Container>
                 <div className="input-group searchbox mt-3">
-                        <input type="text" className="form-control w3-animate-zoom" placeholder="Zipcode | Doctors Name"/>
+                        <input type="text" className="form-control w3-animate-zoom" placeholder="Zipcode | Doctors Name" onChange={(e)=>{
+                                setKeyword(e.target.value);
+                            }}/>
                     <div className="input-group-append">
-                        <button className="btn btn-secondary w3-animate-zoom" type="button">
+                        <button className="btn btn-secondary w3-animate-zoom" type="button" onClick={searchProduct}>
                         <FaSearch/>
                         </button>
                     </div>
@@ -505,132 +536,46 @@ const Doctorconsultation = () => {
 
                     {/* full length card */}
                     <div className="container" align="center">
-                        <div className="col-lg-12 col-12  card">
-
-                            <div className="row mt-3">
-                                <div className="col-lg-4 col-12 doctordetail" style={{ padding: "10px" }} align="center">
-                                    <img src={doctor3} className="d-block mt-3 doctorphoto cardgroup" alt="medicine" />
-                                </div>
-                                <div className="col-lg-8 col-12" align="center">
-
-                                    <div className="card-body mt-3" align="left">
-                                        <h5 className="card-title">Dr. Joanna</h5>
-                                        <h6>MD,AIIMS Chandigarh</h6>
-
-                                        <p className="card-text description">
-                                            Senior Neurologist,<br /> Apollo Hospital,Kankarbagh,Patna-800020
-
-                                        </p>
-                                        <p style={{ padding: "3px" }}>
-                                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio accusamus cum neque cumque veniam repellendus nihil corrupti, earum delectus laborum tempore perspiciatis autem exercitationem? Voluptas corrupti, ut laborum repudiandae nihil repellat quae inventore ad blanditiis delectus deleniti, provident vitae magnam voluptatum eius, dolor facilis tempora minus fuga. Quidem, facilis illum.
-                                        </p>
+                        {
+                           loading? 
+                           data.map(item=>
+                          (
+                               <div className="col-lg-12 col-12  card">
+                                <div className="row mt-3">
+                                    <div className="col-lg-4 col-12 doctordetail" style={{ padding: "10px" }} align="center">
+                                        <img src={item.imageUrl} className="d-block mt-3 doctorphoto cardgroup" alt="medicine" />
                                     </div>
+                                    <div className="col-lg-8 col-12" align="center">
+                                        <div className="card-body mt-3" align="left">
+                                            <h5 className="card-title">{item.name}</h5>
+                                            
+                                            <p className="card-text description">
+                                            {item.about},<br /> {item.address}
 
-                                    <div align="right">
+                                            </p>
+                                            {/*<p style={{ padding: "3px" }}>
+                                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio accusamus cum neque cumque veniam repellendus nihil corrupti, earum delectus laborum tempore perspiciatis autem exercitationem? Voluptas corrupti, ut laborum repudiandae nihil repellat quae inventore ad blanditiis delectus deleniti, provident vitae magnam voluptatum eius, dolor facilis tempora minus fuga. Quidem, facilis illum.
+                                           </p>*/}
+                                        </div>
 
-                                        <button type="button" className="btn btn-primary seemore1 mx-3 my-2">Book Appointment</button>
+                                        <div align="right">
+
+                                            <button type="button" className="btn btn-primary seemore1 mx-3 my-2">Book Appointment</button>
+
+
+                                        </div>
+                                        {/* <InfoModal/> */}
 
 
                                     </div>
-                                    {/* <InfoModal/> */}
-
-
                                 </div>
-                            </div>
-                        </div>
-
+                            </div>)    
+                        ):<Spinner animation="border" style={style}/>}
                     </div>
-                    {/* full length card */}
-                    <div className="container" align="center">
-                        <div className="col-lg-12 col-12 my-2 card">
-
-                            <div className="row mt-3">
-
-                                <div className="col-lg-8 col-12" align="center" >
-
-                                    <div className="card-body mt-3 rightphotodoc" align="left">
-                                        <h5 className="card-title">Dr. Sophia</h5>
-                                        <h6>MD,AIIMS Chandigarh</h6>
-
-                                        <p className="card-text description">
-                                            Senior Neurologist,<br /> Apollo Hospital,Kankarbagh,Patna-800020
-
-                                        </p>
-                                        <p style={{ padding: "3px" }}>
-                                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio accusamus cum neque cumque veniam repellendus nihil corrupti, earum delectus laborum tempore perspiciatis autem exercitationem? Voluptas corrupti, ut laborum repudiandae nihil repellat quae inventore ad blanditiis delectus deleniti, provident vitae magnam voluptatum eius, dolor facilis tempora minus fuga. Quidem, facilis illum.
-                                        </p>
-                                    </div>
-
-                                    <div align="right">
-
-                                        <button type="button" className="btn btn-primary seemore1 mx-3 my-2">Book Appointment</button>
-
-
-                                    </div>
-
-                                    {/* <InfoModal/> */}
-
-
-
-
-
-                                </div>
-                                <div className="col-lg-4 col-12 doctordetail doctorphoto" align="center">
-                                    <img src={doctor1} className="d-block mt-3 cardgroup" alt="medicine" />
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                    {/* full length card */}
-                    <div className="container" align="center">
-                        <div className="col-lg-12 col-12 my-2 card">
-
-                            <div className="row mt-3">
-                                <div className="col-lg-4 col-12 doctordetail doctorphoto" style={{ padding: "5px" }} align="center">
-                                    <img src={doctor2} className="d-block mt-3 cardgroup" alt="medicine" />
-                                </div>
-                                <div className="col-lg-8 col-12" align="center">
-
-                                    <div className="card-body mt-3" align="left">
-                                        <h5 className="card-title">Dr. Joanna</h5>
-                                        <h6>MD,AIIMS Chandigarh</h6>
-
-                                        <p className="card-text description">
-                                            Senior Neurologist,<br /> Apollo Hospital,Kankarbagh,Patna-800020
-
-                                        </p>
-                                        <p style={{ padding: "3px" }}>
-                                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio accusamus cum neque cumque veniam repellendus nihil corrupti, earum delectus laborum tempore perspiciatis autem exercitationem? Voluptas corrupti, ut laborum repudiandae nihil repellat quae inventore ad blanditiis delectus deleniti, provident vitae magnam voluptatum eius, dolor facilis tempora minus fuga. Quidem, facilis illum.
-                                        </p>
-                                    </div>
-
-                                    <div align="right">
-
-                                        <button type="button" className="btn btn-primary seemore1 mx-3 my-2">Book Appointment</button>
-
-                                    </div>
-
-
-                                    {/* <InfoModal/> */}
-
-
-
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-
-
-
-                </div>
+               </div>
             </div>
             <Footer />
-
-
-        </>
+       </>
     )
 }
 
