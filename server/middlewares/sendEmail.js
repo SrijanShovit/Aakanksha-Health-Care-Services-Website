eadee0c0aa94b5514e1668f8ecb2f6dcb307eaed
Shubham Jha <shubhamjha03563@gmail.com>
2021-10-13 08:54:59 +0530

    'gmail-service'

--- server/middlewares/sendEmail.js
+++ server/middlewares/sendEmail.js
@@ -4,22 +4,21 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    /*
-      For Gmail -
-      service: 'gmail',
-      auth:{
-        user: process.env.EMAIL_ADDRESS,
-        pass: process.env.EMAIL_PASSWORD,
-      }
-    */
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASSWORD,
+    },
 
-    // testing with mailtrap
+    /*
+    For testing with mailtrap
     host: 'smtp.mailtrap.io',
     port: process.env.MAILTRAP_PORT,
     auth: {
       user: process.env.MAILTRAP_ID,
       pass: process.env.MAILTRAP_PASSWORD,
     },
+    */
   });
 
   const message = {
