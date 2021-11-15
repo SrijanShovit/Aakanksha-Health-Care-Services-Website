5cfb216ad8f3fea6233305d124d722e51509b4b2
saranshbagga <76037795+saranshbagga@users.noreply.github.com>
2021-11-15 18:44:03 +0530

    FAQ

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,99 +1,57 @@
-import React,{useState} from 'react';
+import React from 'react'
 import {Container,Row,Col} from "react-bootstrap";
-import { FaFacebook,FaLinkedin,FaInstagramSquare,FaTwitterSquare,FaYoutubeSquare } from "react-icons/fa";
-import {Link} from 'react-router-dom'
-import Axios from 'axios';
-
+import { FaFacebook,FaLinkedin,FaInstagramSquare,FaTwitterSquare } from "react-icons/fa";
+import F from "../Pages/FAQ"
+import { Link } from "react-router-dom";
 const Footer = () => {
-
-  const [resmsg,setResMsg]=useState("");
-  //function to send email to backend on clicking button
-  const subscribeHandler = () => {
-    const email = window.sessionStorage.getItem("email");
-    Axios.post('',
-            {
-                "email": email,
-               
-            })
-            .then((response)=>
-            {
-            
-                setResMsg(response.data.message);
-            })
-  }
     return (
     <>
       <div className="footer p-4 w3-animate-zoom bg-dark text-light"  fluid="lg">
         <Row>
           <Col sm>
-            
-              <h6 className="text-uppercase font-weight-bold text-center text-light">About</h6>
+              <h6 className="text-uppercase font-weight-bold text-center">About</h6>
               <hr className="bg-success mb-1 mt-0 d-inline-block mx-auto"  />
-              <ul>
-                <li><a href="/aboutcompany"><h6 className='text-light'>
-                About Aakanksha Company
-                  </h6></a> </li>
-              </ul>
-              <ul>
-                <li><a href="/about"><h6 className='text-light'>
-                About Aakanksha's Healthcare Services
-                  </h6></a> </li>
-              </ul>
               <p className="mt-2">AAKANKSHA is an MSME consumer service E-commerce company started with the intention of making
                 people's life more comfortable. We provide a door-to-door delivery service facility .We provide a wide range
                 of services and products.</p>
-              <a href="/about">More.....</a>  
-              <br />
-              <br />
-              
-               
+              <a href="/about">More.....</a>
           </Col>
           <Col sm>
-              <h6 className="text-uppercase font-weight-bold text-center text-light">Features</h6>
+              <h6 className="text-uppercase font-weight-bold text-center">Features</h6>
               <hr className="bg-success mb-1 mt-0 d-inline-block mx-auto" />
               <ul >
                 <li className="my-2"><a href="#">Medical Safety Product</a></li>
                 <li className="my-2"><a href="#">Nutritious Product</a></li>
                 <li className="my-2"> <a href="#">Top Medicine Brands</a></li>
+
               </ul>
           </Col>
           <Col sm>
-              <h6 className="text-uppercase font-weight-bold text-center text-light">Useful links</h6>
+              <h6 className="text-uppercase font-weight-bold text-center">Useful links</h6>
               <hr className="bg-success mb-1 mt-0 d-inline-block mx-auto"  />
               <ul>
-                <li className="my-2"><Link to="/medicostore">Knock Knock Medico</Link></li>
+                <li className="my-2"><a href="#">Knock Knock Medico</a></li>
                 <li className="my-2"><a href="#">Doctors' Consultation</a></li>
                 <li className="my-2"><a href="#">Medical Camp Location</a></li>
-               
+                <li><Link to="/FAQ">FAQ</Link></li>
               </ul>
           </Col>
           <Col sm>
-          <h6 className="text-uppercase font-weight-bold text-center text-light">Contact</h6>
+          <h6 className="text-uppercase font-weight-bold text-center">Contact</h6>
                 <hr className="bg-success mb-1 mt-0 d-inline-block mx-auto"  />
                 <ul>
                 <li className="my-2"> <i className="fa fa-home  mr-3" aria-hidden="true" /> Delhi,India</li>
                 <li className="my-2"><i className="fa fa-envelope" aria-hidden="true" /> helthcare@gmail.com</li>
                 <li className="my-2"><i className="fa fa-phone" aria-hidden="true" /> + 01 234 567 88</li>
-                <Link to='/reachout'><li className="my-2">Reach Out To Us</li></Link>
-                <br />
-                <button type="button" className="btn btn-light"
-                onClick={()=>subscribeHandler}
-                >Subscribe to our newsletter</button>
-                
-                
               </ul>
-             
           </Col>
-    
+
         </Row>
         <Row>
         <Container><hr className="bg-success" /></Container>
           <Col  sm >
             <p>Terms and conditions</p>
-          </Col> 
-          <Col  sm >
-            <p>Privacy policy</p>
-          </Col> 
+          </Col>
           <Col sm>
             <p>©2021 Aakanksha.
             <a href="#">All Rights Reserved.</a>
@@ -102,27 +60,22 @@ const Footer = () => {
           <Col sm>
             <Row className="social-media">
               <Col>
-              
                 <FaFacebook color="#3b5998"/>
-                
               </Col>
               <Col>
-              <a href="https://www.instagram.com/aakankshacompany/"><FaInstagramSquare color="#fb3958"/></a>
-                 
+                 <FaLinkedin color="#0e76a8"/>
               </Col>
               <Col>
-              <a href="https://twitter.com/AAKANKS57852311"><FaTwitterSquare color="#00acee"/></a>
-                
+                 <FaInstagramSquare color="#fb3958"/>
               </Col>
               <Col>
-              <a href="https://www.youtube.com/channel/UCQ79LZ_IrnxzkFL5MZxxOeg"><FaYoutubeSquare color="red"/></a>
-                
+                <FaTwitterSquare color="#00acee"/>
               </Col>
             </Row>
           </Col>
         </Row>
      </div>
-     
+
     </>
     )
 }
